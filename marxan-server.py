7a7d9b557a2f802d577c5657043898799213a7f6
Andrew Cottam <a.cottam@gmail.com>
2020-02-04 11:08:38 +0000

    v0.9.3 build

--- marxan-server.py
+++ marxan-server.py
@@ -36,7 +36,7 @@ ROLE_UNAUTHORISED_METHODS = {
     "User": ["testRoleAuthorisation","deleteFeature","getUsers","deleteUser","deletePlanningUnitGrid","getRunLogs","clearRunLogs","updateWDPA"],
     "Admin": []
 }
-MARXAN_SERVER_VERSION = "v0.9.29"
+MARXAN_SERVER_VERSION = "v0.9.3"
 MARXAN_REGISTRY = "https://andrewcottam.github.io/marxan-web/registry/marxan.js"
 GUEST_USERNAME = "guest"
 NOT_AUTHENTICATED_ERROR = "Request could not be authenticated. No secure cookie found."

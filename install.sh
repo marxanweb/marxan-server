421185cfa9b28a8c3c7bfdb0a688e7cbeebbfd3a
Andrew Cottam <a.cottam@gmail.com>
2020-03-21 15:37:30 +0000

    new install3

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ bash ./Miniconda3-latest-Linux-x86_64.sh -b -p ./miniconda3
 #remove the installer
 rm ./Miniconda3-latest-Linux-x86_64.sh 
 #initialise so we can use conda from bash (this is for the root user)
-sudo su -c "/home/a_cottam/miniconda3/bin/conda init bash"
+sudo su -c "./miniconda3/bin/conda init bash"
 #set the base environment
 ./miniconda3/bin/conda activate base
 ### PYTHON PREREQUISITES

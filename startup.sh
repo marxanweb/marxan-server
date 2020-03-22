aca61f41d96979ae111c210b5c8c80a0fc9a86fd
Andrew Cottam <a.cottam@gmail.com>
2020-03-22 15:25:47 +0000

    install 11

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
+#!/bin/bash -i
+#running in an interactive bash session to load the .bashrc script
 #run conda activate base - this sets environment variables that are needed e.g. GDAL_DATA
-/home/a_cottam/miniconda3/bin/conda activate base
+${CONDA_EXE} activate base
 #start marxan-server using a detached screen session that can be resumed
-screen -d -m /home/a_cottam/miniconda3/bin/python /home/a_cottam/marxan-server/marxan-server.py
+screen -d -m ${CONDA_PYTHON_EXE} /home/a_cottam/marxan-server/marxan-server.py

ef14ee0f383f2480612f825edd30ca04f7463105
Andrew Cottam <a.cottam@gmail.com>
2020-03-22 11:19:44 +0000

    added install_root.sh

---
+++ install_root.sh
@@ -0,0 +1,36 @@
+## MINICONDA
+#download the miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+#installs miniconda silently
+bash ./Miniconda3-latest-Linux-x86_64.sh -b -p ./miniconda3
+#remove the installer
+rm ./Miniconda3-latest-Linux-x86_64.sh 
+#initialise so we can use conda from bash
+./miniconda3/bin/conda init bash
+### PYTHON PREREQUISITES
+#install the python prerequisites silently
+./miniconda3/bin/conda install -y tornado psycopg2 pandas gdal colorama psutil sqlalchemy    
+./miniconda3/bin/pip install mapbox aiopg aiohttp -q
+### POSTGRESQL/POSTGIS
+#install postgresql/postgis
+apt-get update  
+apt-get install postgresql-10 postgis -y
+apt-get update  
+#create the postgis extensions
+sudo -u postgres psql -c "CREATE EXTENSION postgis;"
+sudo -u postgres psql -c "CREATE EXTENSION postgis_topology;"
+### MARXAN DATABASE
+#create the jrc user
+sudo -u postgres psql -c "CREATE USER jrc WITH PASSWORD 'thargal88' LOGIN NOSUPERUSER IN GROUP postgres;"
+#create the marxanserver database
+sudo -u postgres psql -c "CREATE DATABASE marxanserver WITH TEMPLATE = template0 ENCODING='UTF8';"
+#get the database dump 
+wget https://github.com/andrewcottam/marxan-server/releases/download/Beta2/dump.sql 
+#restore the database
+sudo -u postgres pg_restore ./dump.sql -d marxanserver
+#remove dump file
+rm ./dump.sql   
+#create the default server.dat file for the server configuration
+cp ./marxan-server/server.dat.default ./marxan-server/server.dat
+
+echo "Installed."

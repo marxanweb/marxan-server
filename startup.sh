4e42cbd2209d929f643f3af1003ece1c25e2b83c
Andrew Cottam <a.cottam@gmail.com>
2020-03-22 10:53:55 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 sudo service postgresql start
-sudo ./miniconda3/bin/python ./marxan-server/marxan-server.py 
+sudo ${CONDA_PYTHON_EXE} ./marxan-server/marxan-server.py 
 #screen -d -m /home/a_cottam/miniconda3/bin/python /home/a_cottam/marxan-server/marxan-server.py

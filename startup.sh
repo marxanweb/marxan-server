6fd5dda43f58daa6d96064c2dd047a020daf65dd
Andrew Cottam <a.cottam@gmail.com>
2020-04-23 16:45:54 +0000

    improved startup

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -i
-#running in an interactive bash session to load the .bashrc script
-#run conda activate base - this sets environment variables that are needed e.g. GDAL_DATA
-${CONDA_EXE} activate base
-# on unix the following may be required
-#sudo service postgresql start
+#import the environment variables for marxan-server
+. /etc/profile.d/marxan-server.sh
+conda activate base 
 #start marxan-server using a detached screen session that can be resumed
-screen -d -m ${CONDA_PYTHON_EXE} ${MARXAN_SERVER_DIRECTORY}\/marxan-server.py
+# screen -d -m ${CONDA_PYTHON_EXE} ${MARXAN_SERVER_DIRECTORY}\/marxan-server.py
+#for testing - this will output to stdout
+${CONDA_PYTHON_EXE} ${MARXAN_SERVER_DIRECTORY}\/marxan-server.py

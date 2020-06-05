5f5b7733a6707bc8532d0b276e91e04db7be72c6
Andrew Cottam <a.cottam@gmail.com>
2020-06-05 13:50:08 +0000

    version update

--- marxan-server.py
+++ marxan-server.py
@@ -38,7 +38,7 @@ ROLE_UNAUTHORISED_METHODS = {
     "User": ["testRoleAuthorisation","deleteFeature","getUsers","deleteUser","deletePlanningUnitGrid","clearRunLogs","updateWDPA","toggleEnableGuestUser","shutdown","addParameter","block", "resetDatabase","cleanup",'runSQLFile'],
     "Admin": []
 }
-MARXAN_SERVER_VERSION = "v0.9.4"
+MARXAN_SERVER_VERSION = "v0.9.5"
 MARXAN_LOG_FILE = 'marxan-server.log'
 MARXAN_REGISTRY = "https://marxanweb.github.io/general/registry/marxan.js"
 GUEST_USERNAME = "guest"
